8525fd2a1805c6e6b98c34f4434141752c413710
tBaronDar <thedarelis@gmail.com>
2023-11-04 19:16:42 +0100

    Start

---
+++ components/Welcome.js
@@ -0,0 +1,5 @@
+function Welcome() {
+  return <h1>Wlecome</h1>;
+}
+
+export default Welcome;
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import Welcome from "../components/Welcome";
+
+function HomePage() {
+  return <Welcome />;
+}
+
+export default HomePage;
