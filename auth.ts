c6c3ff0b6ef20353f3cec21e5fe97bb2e5c5526d
tBaronDar <thedarelis@gmail.com>
2024-11-25 16:45:26 +0200

    edited auth.ts

--- auth.ts
+++ auth.ts
@@ -3,5 +3,17 @@ import GitHub from "next-auth/providers/github";
 import LinkedIn from "next-auth/providers/linkedin";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-	providers: [GitHub, LinkedIn],
+	providers: [
+		GitHub,
+		LinkedIn({
+			clientId: process.env.AUTH_LINKEDIN_ID,
+			clientSecret: process.env.AUTH_LINKEDIN_SECRET,
+		}),
+	],
+	callbacks: {
+		async redirect({ url, baseUrl }) {
+			console.log("Redirect ", url);
+			return baseUrl;
+		},
+	},
 });
